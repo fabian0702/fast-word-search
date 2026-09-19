d7871ede78321a5df25ab0b3ff1a2576ee7faff5
fabian0702 <48295869+fabian0702@users.noreply.github.com>
2026-09-19 21:43:37 +0200

    move shards to /dev/shm to get better performance

--- src/shards.hpp
+++ src/shards.hpp
@@ -68,7 +68,7 @@ class ShardsManager
     std::vector<int> fds;
     std::vector<std::mutex> mutexes;
 
-    std::string base_directory = "./tmp/";
+    std::string base_directory = "/dev/shm/shards/";
 
 public:
     std::vector<int> non_empty_shards;
